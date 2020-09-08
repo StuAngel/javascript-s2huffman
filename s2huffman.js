d4f9c5c4001d308f211791cf2f0fb391bbe5b1d6
Stuart Clement <stuart@stuangel.com>
2020-09-08 14:21:58 +1000

    Update s2huffman.js
    modified line 28 - was padding instead of trimming

--- s2huffman.js
+++ s2huffman.js
@@ -11,8 +11,7 @@ function s2huffman()
 		for(var i in t){ o.push([t[i], i]); t[i]=''; };
 		while(o.length&&(o = o.sort(function(a,b){ return b[0]-a[0]; })))
 			if((result = o.pop())&&(_r = o.pop()))o.push([result[0]+_r[0], result.slice(1), _r.slice(1)]);
-		result = result.slice(1);
-		this.clean(result, t, '');
+		result = result.slice(1); this.clean(result, t, '');
 		var len = (s = s.map(x=>t[x]).join('')).length;
 		while(node = s.substr(-8)){ h = parseInt(node, 2).toString(16).padStart(2, '0').toUpperCase()+h; s = s.substr(0, s.length-8); };
 		r = JSON.stringify(result); for(var i in (p={'\x22':'', ',':'M', '\\\[\\\[':'L', '\\\]\\\]':'K', '\\\(\\\(':'J', '\\\)\\\)':'I', '\\\(\\\[':'H', '\\\)\\\]':'G'}))r = r.replace(new RegExp(i, 'g'), p[i]);
@@ -26,7 +25,7 @@ function s2huffman()
 			for(var i in (p={'G':')]', 'H':'([', 'I':'))', 'J':'((', 'K':']]', 'L':'[[', 'M':'","', '\\\]':'"]', '\\\]\\\x22':']', '\\\[':'["', '\\\x22\\\[':'['}))e[0] = e[0].replace(new RegExp(i, 'g'), p[i]);
 			if(e[0]=JSON.parse(e[0]))
 			{				
-				var lu = e[1].substr(4).match(/.{1,2}/g).map(x=>parseInt(x, 16).toString(2).padStart(8, '0')).join('').padStart(parseInt(e[1].substr(0, 4), 16), '0').split('');
+				lu = (lu = e[1].substr(4).match(/.{1,2}/g).map(x=>parseInt(x, 16).toString(2).padStart(8, '0')).join('')).substr(lu.length-parseInt(e[1].substr(0, 4), 16));
 				var ptr = e[0]; for(var i = 0; i<lu.length; i++)if('string'==typeof ptr[lu[i]]){ result+=String.fromCharCode(parseInt(ptr[lu[i]], 16)); ptr = e[0]; } else ptr = ptr[lu[i]];
 			};
 		};
